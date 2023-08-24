76d8cc41c6937778f0e492dba4fdde852bdc4887
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2023-08-24 14:51:04 +0530

    Added bird and gravity in the game

---
+++ bird.js
@@ -0,0 +1,25 @@
+
+class Bird
+{
+    constructor()
+    {
+        this.bird = document.createElement( 'img' );
+        this.bird.src = './resources/images/bird.png';
+
+        this.x = ( g_virtual_width / 2 ) - ( this.bird.width / 2 );
+        this.y = ( g_virtual_height / 2 ) - ( this.bird.height / 2 );
+
+        this.dy = 0;
+    }
+
+    update( dt )
+    {
+        this.dy = this.dy + ( dt * g_gravity );
+        this.y = this.y + ( dt * this.dy );
+    }
+
+    draw()
+    {
+        g_game.drawImage( this.bird, this.x, this.y );
+    }
+}
