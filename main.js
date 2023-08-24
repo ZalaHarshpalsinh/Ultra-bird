3859ea6e40f64c589fc9e74be6e1e8c40a814d8b
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2023-08-24 14:50:37 +0530

    Added bird and gravity in the game

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var g_game_area = document.querySelector( "#game_area" );
-
 var g_game = g_game_area.getContext( '2d' );
 
 var g_actual_width = g_game_area.width;
@@ -9,30 +8,36 @@ var g_virtual_height = 288;
 
 g_game.scale( g_actual_width / g_virtual_width, g_actual_height / g_virtual_height );
 g_game.imageSmoothingEnabled = false;
+setInterval( render, 16.667 );
 
 
 let last_update = Date.now();
-let backgrounds = new Backgrounds();
+const g_gravity = 35952;
 
+
+let backgrounds = new Backgrounds();
+let bird = new Bird();
 render();
 function render()
 {
     let now = Date.now();
     let dt = ( ( now - last_update ) / 1000 );
     last_update = now;
+
     update( dt );
     draw();
-    window.requestAnimationFrame( render );
 }
 
 function update( dt )
 {
     backgrounds.update( dt );
+    bird.update( dt );
 }
 
 function draw()
 {
     g_game.clearRect( 0, 0, g_actual_width, g_actual_height );
     backgrounds.draw();
+    bird.draw();
 }
 
